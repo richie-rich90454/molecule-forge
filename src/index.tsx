a4731aaef8feac361af07d7e264cddcd8d1cb30e
richie-rich90454 <rjiang880@gmail.com>
2026-09-14 19:53:07 +0800

    feat(app): load the wasm module at startup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import "./fonts.css";
 import "./styles.css";
 import { Application } from "./Application";
+import { loadWasmModule } from "./wasm/loadWasm";
 
 const root = document.getElementById("root");
 
@@ -8,5 +9,5 @@ if (root === null) {
     throw new Error("Root element not found");
 }
 
-const application = new Application(root);
+const application = new Application(root, { loadWasmModule });
 application.start();

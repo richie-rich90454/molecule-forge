3dfe82bfdac22b331778e366d9c03f5702febe8c
richie-rich90454 <rjiang880@gmail.com>
2026-09-14 19:53:06 +0800

    feat(wasm): add the web module loader

---
+++ src/wasm/loadWasm.ts
@@ -0,0 +1,7 @@
+import init, { compute_forces } from "./engine/pkg/molecule_forge_engine.js";
+import type { IWasmForceModule } from "../sim/ForceField";
+
+export async function loadWasmModule(): Promise<IWasmForceModule | null> {
+    await init();
+    return { compute_forces };
+}
